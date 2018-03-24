5edc57a44052698665d067f29edc68ee59e9ac41
Jovi <redzjovi@gmail.com>
2018-03-24 11:19:10 +0700

    Remove #permissions.
    - Remove permissions, and set permissions from module.

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,8 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('backend/pages/{id}/trash', ['as' => 'backend.pages.trash', 'uses' => 'Backend\PagesController@trash']);
     });
     Route::group(['middleware' => ['permission:backend permissions']], function () {
-        Route::resource('backend/permissions', 'Backend\PermissionsController', ['as' => 'backend']);
-        Route::get('backend/permissions/{id}/delete', ['as' => 'backend.permissions.delete', 'uses' => 'Backend\PermissionsController@delete']);
+        Route::resource('backend/permissions', '\Modules\Permissions\Http\Controllers\Backend\PermissionsController', ['as' => 'backend']);
+        Route::get('backend/permissions/{id}/delete', ['as' => 'backend.permissions.delete', 'uses' => '\Modules\Permissions\Http\Controllers\Backend\PermissionsController@delete']);
     });
     Route::group(['middleware' => ['permission:backend posts']], function () {
         Route::resource('backend/posts', 'Backend\PostsController', ['as' => 'backend']);
