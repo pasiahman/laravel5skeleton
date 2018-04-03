68564559147539ab5777c70b13cc8bcc63da693a
Jovi <redzjovi@gmail.com>
2018-04-03 11:09:46 +0700

    Update #modules, #permissions.
    - Move permissions seeder to module.

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -14,37 +14,6 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run()
     {
-        // Reset cached roles and permissions
-        app()['cache']->forget('spatie.permission.cache');
-
-        // create permissions
-        Permission::create(['name' => 'backend categories']);
-        Permission::create(['name' => 'backend custom links']);
-        Permission::create(['name' => 'backend custom links delete']);
-        Permission::create(['name' => 'backend custom links trash']);
-        Permission::create(['name' => 'backend dashboard']);
-        Permission::create(['name' => 'backend main']);
-        Permission::create(['name' => 'backend masters']);
-        Permission::create(['name' => 'backend media']);
-        Permission::create(['name' => 'backend media all']);
-        Permission::create(['name' => 'backend media delete']);
-        Permission::create(['name' => 'backend media trash']);
-        Permission::create(['name' => 'backend media role']);
-        Permission::create(['name' => 'backend medium categories']);
-        Permission::create(['name' => 'backend menus']);
-        Permission::create(['name' => 'backend options']);
-        Permission::create(['name' => 'backend permissions']);
-        Permission::create(['name' => 'backend pages']);
-        Permission::create(['name' => 'backend pages delete']);
-        Permission::create(['name' => 'backend pages trash']);
-        Permission::create(['name' => 'backend posts']);
-        Permission::create(['name' => 'backend posts delete']);
-        Permission::create(['name' => 'backend posts trash']);
-        Permission::create(['name' => 'backend roles']);
-        Permission::create(['name' => 'backend tags']);
-        Permission::create(['name' => 'backend user addresses']);
-        Permission::create(['name' => 'backend users']);
-
         // create roles and assign existing permissions
         $role = Role::create(['name' => 'superadmin']);
         $role->givePermissionTo([
