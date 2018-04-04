5d25203ced56ef438c8323ab2cc8468d7bdb8076
Jovi <redzjovi@gmail.com>
2018-04-04 17:01:38 +0700

    #rajaongkir, update.
    - Add api/rajaongkir/cost.

--- database/seeds/RolesAndPermissionsSeeder.php
+++ database/seeds/RolesAndPermissionsSeeder.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Seeder;
 use Modules\Users\Models\Users;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
@@ -15,39 +14,9 @@ class RolesAndPermissionsSeeder extends Seeder
     public function run()
     {
         // create roles and assign existing permissions
-        $role = Role::create(['name' => 'superadmin']);
-        $role->givePermissionTo([
-            'backend categories',
-            'backend custom links',
-            'backend custom links delete',
-            'backend custom links trash',
-            'backend dashboard',
-            'backend geocodes',
-            'backend main',
-            'backend masters',
-            'backend media',
-            'backend media all',
-            'backend media delete',
-            'backend media role',
-            'backend media trash',
-            'backend medium categories',
-            'backend menus',
-            'backend options',
-            'backend permissions',
-            'backend pages',
-            'backend pages delete',
-            'backend pages trash',
-            'backend posts',
-            'backend posts delete',
-            'backend posts trash',
-            'backend roles',
-            'backend tags',
-            'backend user addresses',
-            'backend users',
-        ]);
-
         $role = Role::create(['name' => 'admin']);
         $role->givePermissionTo([
+            'api rajaongkir',
             'backend categories',
             'backend custom links',
             'backend dashboard',
